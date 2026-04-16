6e7e18705eda67d24dfd95bb4e82811d6be65df7
unsatisfiedsoul <ronyhassan091101@gmail.com>
2026-04-16 10:00:00 +0600

    Add BugHunting folder

---
+++ BugHunting/Lansweeper.com/lansweeper_wplogin_redirect_authorize_curl_full.php
@@ -0,0 +1,142 @@
+HTTP/2 200 
+date: Mon, 13 Apr 2026 07:33:18 GMT
+content-type: text/html; charset=UTF-8
+expires: Wed, 11 Jan 1984 05:00:00 GMT
+set-cookie: wordpress_test_cookie=WP%20Cookie%20check; path=/; secure; HttpOnly
+set-cookie: wordpress_24912dd7608565da50c41a560ecc0588=%20; expires=Sun, 13 Apr 2025 07:33:18 GMT; Max-Age=0; path=/wp-admin; secure
+set-cookie: wordpress_sec_24912dd7608565da50c41a560ecc0588=%20; expires=Sun, 13 Apr 2025 07:33:18 GMT; Max-Age=0; path=/wp-admin; secure
+set-cookie: wordpress_24912dd7608565da50c41a560ecc0588=%20; expires=Sun, 13 Apr 2025 07:33:18 GMT; Max-Age=0; path=/wp-content/plugins; secure
+set-cookie: wordpress_sec_24912dd7608565da50c41a560ecc0588=%20; expires=Sun, 13 Apr 2025 07:33:18 GMT; Max-Age=0; path=/wp-content/plugins; secure
+set-cookie: wordpress_logged_in_24912dd7608565da50c41a560ecc0588=%20; expires=Sun, 13 Apr 2025 07:33:18 GMT; Max-Age=0; path=/; secure
+set-cookie: wordpress_logged_in_24912dd7608565da50c41a560ecc0588=%20; expires=Sun, 13 Apr 2025 07:33:18 GMT; Max-Age=0; path=/; secure
+set-cookie: wp-settings-0=%20; expires=Sun, 13 Apr 2025 07:33:18 GMT; Max-Age=0; path=/; secure
+set-cookie: wp-settings-time-0=%20; expires=Sun, 13 Apr 2025 07:33:18 GMT; Max-Age=0; path=/; secure
+set-cookie: wordpress_24912dd7608565da50c41a560ecc0588=%20; expires=Sun, 13 Apr 2025 07:33:18 GMT; Max-Age=0; path=/; secure
+set-cookie: wordpress_24912dd7608565da50c41a560ecc0588=%20; expires=Sun, 13 Apr 2025 07:33:18 GMT; Max-Age=0; path=/; secure
+set-cookie: wordpress_sec_24912dd7608565da50c41a560ecc0588=%20; expires=Sun, 13 Apr 2025 07:33:18 GMT; Max-Age=0; path=/; secure
+set-cookie: wordpress_sec_24912dd7608565da50c41a560ecc0588=%20; expires=Sun, 13 Apr 2025 07:33:18 GMT; Max-Age=0; path=/; secure
+set-cookie: wordpressuser_24912dd7608565da50c41a560ecc0588=%20; expires=Sun, 13 Apr 2025 07:33:18 GMT; Max-Age=0; path=/; secure
+set-cookie: wordpresspass_24912dd7608565da50c41a560ecc0588=%20; expires=Sun, 13 Apr 2025 07:33:18 GMT; Max-Age=0; path=/; secure
+set-cookie: wordpressuser_24912dd7608565da50c41a560ecc0588=%20; expires=Sun, 13 Apr 2025 07:33:18 GMT; Max-Age=0; path=/; secure
+set-cookie: wordpresspass_24912dd7608565da50c41a560ecc0588=%20; expires=Sun, 13 Apr 2025 07:33:18 GMT; Max-Age=0; path=/; secure
+set-cookie: wp-postpass_24912dd7608565da50c41a560ecc0588=%20; expires=Sun, 13 Apr 2025 07:33:18 GMT; Max-Age=0; path=/; secure
+set-cookie: country=BD; Path=/; SameSite=Lax
+x-frame-options: SAMEORIGIN,SAMEORIGIN,SAMEORIGIN
+content-security-policy: 
+referrer-policy: no-referrer-when-downgrade
+x-litespeed-cache-control: no-cache
+cache-control: no-cache, must-revalidate, max-age=0, no-store, private
+vary: Accept-Encoding
+server: cloudflare
+x-xss-protection: 1; mode=block
+x-content-type-options: nosniff
+strict-transport-security: max-age=31536000; includeSubDomains; preload
+permissions-policy: geolocation=(), microphone=(), camera=(), fullscreen=(self)
+x-turbo-charged-by: LiteSpeed
+cf-cache-status: DYNAMIC
+cf-ray: 9eb8c8229df35658-DAC
+
+<!DOCTYPE html>
+	<html lang="en-US">
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Log In &lsaquo; Lansweeper &#8212; WordPress</title>
+	<style>
+        #login h1 a {
+            background-image: url(https://www.lansweeper.com/wp-content/uploads/Lansweeper-Full-Logo-Grey-1.svg);
+            height: 4rem;
+            width: 100%;
+            background-size: contain;
+        }
+    </style>
+
+<meta name='robots' content='max-image-preview:large, noindex, noarchive' />
+<link rel='stylesheet' id='dashicons-css' href='https://www.lansweeper.com/wp-includes/css/dashicons.min.css?ver=6.9.4' media='all' />
+<link rel='stylesheet' id='buttons-css' href='https://www.lansweeper.com/wp-includes/css/buttons.min.css?ver=6.9.4' media='all' />
+<link rel='stylesheet' id='forms-css' href='https://www.lansweeper.com/wp-admin/css/forms.min.css?ver=6.9.4' media='all' />
+<link rel='stylesheet' id='l10n-css' href='https://www.lansweeper.com/wp-admin/css/l10n.min.css?ver=6.9.4' media='all' />
+<link rel='stylesheet' id='login-css' href='https://www.lansweeper.com/wp-admin/css/login.min.css?ver=6.9.4' media='all' />
+	<meta name='referrer' content='strict-origin-when-cross-origin' />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="icon" href="https://www.lansweeper.com/wp-content/uploads/cropped-Lansweeper-2-32x32.png" sizes="32x32" />
+<link rel="icon" href="https://www.lansweeper.com/wp-content/uploads/cropped-Lansweeper-2-192x192.png" sizes="192x192" />
+<link rel="apple-touch-icon" href="https://www.lansweeper.com/wp-content/uploads/cropped-Lansweeper-2-180x180.png" />
+<meta name="msapplication-TileImage" content="https://www.lansweeper.com/wp-content/uploads/cropped-Lansweeper-2-270x270.png" />
+	</head>
+	<body class="login no-js login-action-login wp-core-ui  locale-en-us">
+	<script>
+document.body.className = document.body.className.replace('no-js','js');
+</script>
+
+				<h1 class="screen-reader-text">Log In</h1>
+			<div id="login">
+		<h1 role="presentation" class="wp-login-logo"><a href="https://www.lansweeper.com">Powered by WordPress</a></h1>
+	<div id="login-message" class="notice notice-info message"><p>Please log in to Lansweeper to proceed with authorization.</p></div>
+		<form name="loginform" id="loginform" action="https://www.lansweeper.com/wp-login.php" method="post">
+			<p>
+				<label for="user_login">Username or Email Address</label>
+				<input type="text" name="log" id="user_login" aria-describedby="login-message" class="input" value="" size="20" autocapitalize="off" autocomplete="username" required="required" />
+			</p>
+
+			<div class="user-pass-wrap">
+				<label for="user_pass">Password</label>
+				<div class="wp-pwd">
+					<input type="password" name="pwd" id="user_pass" aria-describedby="login-message" class="input password-input" value="" size="20" autocomplete="current-password" spellcheck="false" required="required" />
+					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Show password">
+						<span class="dashicons dashicons-visibility" aria-hidden="true"></span>
+					</button>
+				</div>
+			</div>
+						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> <label for="rememberme">Remember Me</label></p>
+			<p class="submit">
+				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
+									<input type="hidden" name="redirect_to" value="https://lansweeper.com/wp-admin/authorize-application.php" />
+									<input type="hidden" name="testcookie" value="1" />
+			</p>
+		</form>
+
+					<p id="nav">
+				<a class="wp-login-lost-password" href="https://www.lansweeper.com/wp-login.php?action=lostpassword">Lost your password?</a>			</p>
+			<script>
+function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
+wp_attempt_focus();
+if ( typeof wpOnload === 'function' ) { wpOnload() }
+</script>
+		<p id="backtoblog">
+			<a href="https://www.lansweeper.com/">&larr; Go to Lansweeper</a>		</p>
+		<div class="privacy-policy-page-link"><a class="privacy-policy-link" href="https://www.lansweeper.com/about/privacy-policy/" rel="privacy-policy">Privacy Policy</a></div>	</div>
+	
+	<script src="https://www.lansweeper.com/wp-includes/js/clipboard.min.js?ver=2.0.11" id="clipboard-js"></script>
+<script src="https://www.lansweeper.com/wp-includes/js/jquery/jquery.min.js?ver=3.7.1" id="jquery-core-js"></script>
+<script id="zxcvbn-async-js-extra">
+var _zxcvbnSettings = {"src":"https://www.lansweeper.com/wp-includes/js/zxcvbn.min.js"};
+//# sourceURL=zxcvbn-async-js-extra
+</script>
+<script src="https://www.lansweeper.com/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
+<script src="https://www.lansweeper.com/wp-includes/js/dist/hooks.min.js?ver=dd5603f07f9220ed27f1" id="wp-hooks-js"></script>
+<script src="https://www.lansweeper.com/wp-includes/js/dist/i18n.min.js?ver=c26c3dc7bed366793375" id="wp-i18n-js"></script>
+<script id="wp-i18n-js-after">
+wp.i18n.setLocaleData( { 'text direction\u0004ltr': [ 'ltr' ] } );
+//# sourceURL=wp-i18n-js-after
+</script>
+<script id="password-strength-meter-js-extra">
+var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"Weak","good":"Medium","strong":"Strong","mismatch":"Mismatch"};
+//# sourceURL=password-strength-meter-js-extra
+</script>
+<script src="https://www.lansweeper.com/wp-admin/js/password-strength-meter.min.js?ver=6.9.4" id="password-strength-meter-js"></script>
+<script src="https://www.lansweeper.com/wp-includes/js/underscore.min.js?ver=1.13.7" id="underscore-js"></script>
+<script id="wp-util-js-extra">
+var _wpUtilSettings = {"ajax":{"url":"/wp-admin/admin-ajax.php"}};
+//# sourceURL=wp-util-js-extra
+</script>
+<script src="https://www.lansweeper.com/wp-includes/js/wp-util.min.js?ver=6.9.4" id="wp-util-js"></script>
+<script src="https://www.lansweeper.com/wp-includes/js/dist/dom-ready.min.js?ver=f77871ff7694fffea381" id="wp-dom-ready-js"></script>
+<script src="https://www.lansweeper.com/wp-includes/js/dist/a11y.min.js?ver=cb460b4676c94bd228ed" id="wp-a11y-js"></script>
+<script id="user-profile-js-extra">
+var userProfileL10n = {"user_id":"0","nonce":"02759c4416"};
+//# sourceURL=user-profile-js-extra
+</script>
+<script src="https://www.lansweeper.com/wp-admin/js/user-profile.min.js?ver=6.9.4" id="user-profile-js"></script>
+	</body>
+	</html>
+	

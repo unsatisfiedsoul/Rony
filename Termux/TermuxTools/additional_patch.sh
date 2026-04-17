6a24d52f8dfce283bedb6058e05911950fc61ea2
unsatisfiedsoul <ronyhassan091101@gmail.com>
2026-04-18 02:35:21 +0600

    Trying to update the changes on Termux marge additional_patch.sh

--- Termux/TermuxTools/additional_patch.sh
+++ Termux/TermuxTools/additional_patch.sh
@@ -5,10 +5,14 @@ echo 'export PATH="$PATH:$HOME/go/bin"' >> ~/.zshrc
 echo 'export PATH="$PATH:$HOME/go/bin"' >> ~/.bashrc
 #source ~/.zshrc
 source ~/.bashrc
+git clone https://github.com/Sohil876/Termux-zsh.git && cd Termux-zsh
+bash setup.sh
 
 echo '------------|Patching msfconsole|---------'
 gem install mini_portile2 -v 2.8.2
 
 cp -r ~/Tools/Metasploit-termux ~/
 bash ~/Metasploit-termux/metasploit.sh
-
+rm -rf ~/Metasploit-termux
+rm -rf ~/Termux-zsh
+rm -rf ~/tools
